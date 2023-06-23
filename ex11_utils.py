d72656c3f3454f1ae3ab731921c9ad83185e6d89
Shahar Tamir <89067508+ShaharTamir1@users.noreply.github.com>
2023-06-23 12:50:46 +0300

    Find length n words (#2)
    * WIP recursive for find_length_n_words
    
    * WIP n_words now kinda works but not really
    
    * n_words actually works now
    
    _is_valid_board_path was also fixed
    
    * n_words + documentation

--- ex11_utils.py
+++ ex11_utils.py
@@ -17,6 +17,11 @@ def _create_words_set(path):
     return words
 
 
+def _cells_in_board(board: Board) -> List[Tuple[int,int]]:
+    """returns an ordered list of all coordinates on the board"""
+    return [(y,x) for x in range(len(board[0])) for y in range(len(board))]
+
+
 def _is_coordinate_in_board_limits(board: Board, coordinate: Location) -> bool:
     """return True if the coordinate is in the board's limits, else False"""
     return (0 <= coordinate[0] < len(board[0])) and (0 <= coordinate[1] < len(board))
@@ -27,24 +32,30 @@ def _check_if_neighbor_cells(cell1: Location, cell2: Location) -> bool:
     return abs(cell1[0] - cell2[0]) <= 1 and abs(cell1[1] - cell2[1]) <= 1
 
 
+def _all_valid_neighbors(cell: Location, board: Board) -> List[Tuple[int, int]]:
+    """return a list of coordinates of all neighboring cells of a given cell
+    In practice each of these neighbors would be a valid step from the cell"""
+    min_x, max_x = max(0, cell[1] - 1), min(len(board[0]), cell[1] + 1)
+    min_y, max_y = max(0, cell[0] - 1), min(len(board), cell[0] + 1)
+    neighbor_list = [(x,y) for x in range(min_x, max_x) for y in range(min_y, max_y)]
+    if cell in neighbor_list:
+        neighbor_list.remove(cell)
+    return neighbor_list
+
+
 def _is_valid_board_path(board: Board, path: Path) -> bool:
-    """return True if every two coordinates are neighbors and in the board limits"""
-    # Make sure the coordinates are in the board, so we won't get an index error when accessing them later
-    is_valid = True
-    for cell_i in range(len(path) - 1):
-        if not _check_if_neighbor_cells(path[cell_i], path[cell_i + 1]) \
-                or not (_is_coordinate_in_board_limits(board, path[cell_i]) and
-                        _is_coordinate_in_board_limits(board, path[cell_i + 1])):
-            is_valid = False
-            break
-    return is_valid
+    """return True if every two coordinates are valid neighbors"""
+    for i in range(len(path)-1):
+        if path[i+1] not in _all_valid_neighbors(path[i], board):
+            return False
+    return True
 
 
 def _get_word_in_path(board: Board, path: Path):
     """connect the letters in the path to a word"""
     word = ""
-    for coordinate in path:
-        word += board[coordinate[0]][coordinate[1]]
+    for cell in path:
+        word += board[cell[0]][cell[1]]
     return word
 
 
@@ -60,7 +71,31 @@ def find_length_n_paths(n: int, board: Board, words: Iterable[str]) -> List[Path
 
 
 def find_length_n_words(n: int, board: Board, words: Iterable[str]) -> List[Path]:
-    pass
+    path_lst = []
+    for y in range(len(board)):
+        for x in range(len(board[0])):
+            path = []
+            _n_words_helper(n, (y, x), board, '', words, path, path_lst)
+    return path_lst
+
+
+def _n_words_helper(n: int, cell: Location, board: Board, word: str, words: Iterable[str], path, path_lst):
+    """starting from a specific cell iterate over all of its possible paths.
+    when a path that assembles an n-long word is found, add it to the path list"""
+    # Add current cell to the word and path
+    path.append(cell)
+    word += board[cell[0]][cell[1]]
+    # Success condition - add the path to the path list
+    if len(word) == n and word in words and path not in path_lst:
+        path_lst.append(path.copy())
+    # Iterate on one of the cell's not-yet-used neighbors
+    if len(word) <= n:
+        for neighbor in _all_valid_neighbors(cell, board):
+            if neighbor not in path:
+                _n_words_helper(n, neighbor, board, word, words, path, path_lst)
+    # Remove the cell from the word and path to backtrack
+    word = word[:-1]
+    path.remove(cell)
 
 
 def max_score_paths(board: Board, words: Iterable[str]) -> List[Path]:
@@ -70,9 +105,9 @@ def max_score_paths(board: Board, words: Iterable[str]) -> List[Path]:
 if __name__ == '__main__':
     words = _create_words_set(WORDS_TXT_DICT_PATH)
     board = randomize_board()
-
-    # #### shahar test:
-    # board = [['D', 'C', 'B', 'A'], ['G', 'U', 'D', 'E'], ['T', 'J', 'Y', 'T'], ['N', 'M', 'F', 'I']] # board from the example for tests
-    # path = [(0,2),(1,3),(1,2)]
-    # path2 = [(3,2),(3,3),(2,3)]
-    # print(is_valid_path(board, path2, words))
+    # TEST
+    print(board)
+    lst = find_length_n_words(5, board, words)
+    print(lst)
+    for path in lst:
+        print(is_valid_path(board, path, words))
