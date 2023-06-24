c1d2bc47ff9e3b2e62636164c24420a2b9ac6001
seanbrg <sean.bkv@gmail.com>
2023-06-23 21:49:50 -0700

    max_score_paths done and optimized + comments everywhere

--- ex11_utils.py
+++ ex11_utils.py
@@ -10,7 +10,7 @@ WORDS_TXT_DICT_PATH = "boggle_dict.txt"
 
 
 def is_valid_path(board: Board, path: Path, words: Iterable[str]) -> Optional[str]:
-    """If the path is valid according to the rules and is a valid word, return the word"""
+    """if the path is valid according to the rules and is a valid word, return the word"""
     if helpers.is_valid_board_path(board, path):
         word_in_path = helpers.get_word_in_path(board, path)
         if word_in_path in words:
@@ -18,45 +18,48 @@ def is_valid_path(board: Board, path: Path, words: Iterable[str]) -> Optional[st
 
 
 def find_length_n_paths(n: int, board: Board, words: Iterable[str]) -> List[Path]:
-    """For every cell in the board iterate a backtracking function that locates all
+    """for every cell in the board iterate a backtracking function that locates all
     n-long paths that create words from the list."""
     path_lst = []
     board_cells = helpers.get_board_cells(board)
     for cell in board_cells:
         path = []
-        helpers.n_length_path(n, cell, board, words, path, path_lst)
+        helpers.n_length('path', '', n, cell, board, words, path, path_lst)
     return path_lst
 
 
 def find_length_n_words(n: int, board: Board, words: Iterable[str]) -> List[Path]:
-    """For every cell in the board iterate a backtracking function that locates all
+    """for every cell in the board iterate a backtracking function that locates all
     paths that create n-long words from the list."""
     path_lst = []
     board_cells = helpers.get_board_cells(board)
     for cell in board_cells:
         path = []
-        helpers.n_length_word(n, cell, board, '', words, path, path_lst)
+        helpers.n_length('word', '', n, cell, board, words, path, path_lst)
     return path_lst
 
 
 def max_score_paths(board: Board, words: Iterable[str]) -> List[Path]:
-
+    """for every cell in the board iterate a backtracking function that locates all
+    paths that create valid words and record the highest scoring path for each word."""
     path_dict = {}
+    words_dict = helpers.create_words_dict(words)
     board_cells = helpers.get_board_cells(board)
+    max_l = list(words_dict.keys())[-1]
     for cell in board_cells:
         path = []
-        helpers.max_score(cell, board, '', words, path, path_dict)
-    path_lst = [i for i in path_dict.values()]
-    return path_lst
+        helpers.max_score(cell, board, '', words_dict, max_l, path, path_dict)
+    # Return the values of the dictionary - all paths found - as a list
+    return list(path_dict.values())
 
 
 if __name__ == '__main__':
     words = helpers.create_words_set(WORDS_TXT_DICT_PATH)
     board = randomize_board()
     # TEST
-    print(board)
-    lst = find_length_n_paths(5, board, words)
+    lst = max_score_paths(board, words)
     print(lst)
-    for path in lst:
+    """for path in lst:
         print(is_valid_path(board, path, words))
+        print(helpers.score(path))"""
 
--- helpers.py
+++ helpers.py
@@ -1,15 +1,11 @@
-from typing import List, Tuple, Iterable, Optional
-from boggle_board_randomizer import randomize_board
-from ex11_utils import is_valid_path
+from typing import List, Tuple, Iterable
 
 Board = List[List[str]]
 Path = List[Tuple[int, int]]
 Location = Tuple[int, int]
 
-WORDS_TXT_DICT_PATH = "boggle_dict.txt"
 
-
-def create_words_set(path):
+def create_words_set(path: str):
     """returns a set with the words in the given file"""
     words = set()
     with open(path, "r") as file:
@@ -18,19 +14,13 @@ def create_words_set(path):
     return words
 
 
-def cells_in_board(board: Board) -> List[Tuple[int, int]]:
-    """returns an ordered list of all coordinates on the board"""
-    return [(y, x) for x in range(len(board[0])) for y in range(len(board))]
-
-
-def is_coordinate_in_board_limits(board: Board, coordinate: Location) -> bool:
-    """return True if the coordinate is in the board's limits, else False"""
-    return (0 <= coordinate[0] < len(board[0])) and (0 <= coordinate[1] < len(board))
-
-
-def check_if_neighbor_cells(cell1: Location, cell2: Location) -> bool:
-    """return True if the cells are next or diagonal to each other"""
-    return abs(cell1[0] - cell2[0]) <= 1 and abs(cell1[1] - cell2[1]) <= 1
+def create_words_dict(words_set: Iterable[str]) -> dict[int:Iterable[str]]:
+    words_dict = {}
+    for i in range(max_length_word(words_set) + 1):
+        words_dict[i] = set()
+    for word in words_set:
+            words_dict[len(word)].add(word)
+    return words_dict
 
 
 def get_board_cells(board: Board) -> Iterable[Location]:
@@ -43,13 +33,12 @@ def get_board_cells(board: Board) -> Iterable[Location]:
 def all_valid_neighbors(cell: Location, board: Board) -> Iterable[Tuple[int, int]]:
     """return a list of coordinates of all neighboring cells of a given cell
     In practice each of these neighbors would be a valid step from the cell"""
-    min_x, max_x = max(0, cell[1] - 1), min(len(board[0]), cell[1] + 1)
-    min_y, max_y = max(0, cell[0] - 1), min(len(board), cell[0] + 1)
+    min_x, max_x = max(0, cell[1] - 1), min(len(board[0])-1, cell[1] + 1)
+    min_y, max_y = max(0, cell[0] - 1), min(len(board)-1, cell[0] + 1)
     neighbor_list = [(x, y) for x in range(min_x, max_x+1) for y in range(min_y, max_y+1)]
     if cell in neighbor_list:
         neighbor_list.remove(cell)
-    # return only neighbors in the board limits:
-    return filter(lambda loc: is_coordinate_in_board_limits(board, loc), neighbor_list)
+    return neighbor_list
 
 
 def is_valid_board_path(board: Board, path: Path) -> bool:
@@ -68,63 +57,64 @@ def get_word_in_path(board: Board, path: Path) -> str:
     return word
 
 
-def score(path: Path):
+def max_length_word(words: Iterable[str]) -> int:
+    """return the length of the longest word"""
+    length = 0
+    for word in words:
+        if length < len(word):
+            length = len(word)
+    return length
+
+
+def score(path: Path) -> int:
     """return the score that would be granted for this path"""
     return len(path) ** 2
 
 
-def n_length_word(n: int, cell: Location, board: Board, word: str, word_lst: Iterable[str], path, path_lst):
+def check_word(target_word: str, words: dict[int:Iterable[str]]) -> bool:
+    """if any word in the dict contains the target word return True"""
+    for word in words[len(target_word)]:
+        if word.startswith(target_word):
+            return True
+    return False
+
+
+def n_length(type, word, n, cell, board, word_lst: Iterable[str], path, path_lst):
     """starting from a specific cell iterate over all of its possible paths.
-    when a path that assembles an n-long word is found, add it to the path list"""
+    when a path that assembles a valid word is found, add it to the path list if path/word length is n"""
     # Add current cell to the word and path
     path.append(cell)
     word += board[cell[0]][cell[1]]
+    # Determine what is being measured
+    length = len(word) if type == 'word' else len(path)
     # Success condition - add the path to the path list
-    if len(word) == n and word in word_lst and path not in path_lst:
+    if length == n and word in word_lst and path not in path_lst:
         path_lst.append(path.copy())
     # Iterate on one of the cell's not-yet-used neighbors
-    if len(word) <= n:
+    if length <= n:
         for neighbor in all_valid_neighbors(cell, board):
             if neighbor not in path:
-                n_length_word(n, neighbor, board, word, word_lst, path, path_lst)
+                n_length(type, word, n, neighbor, board, word_lst, path, path_lst)
     # Remove the cell from the word and path to backtrack
     path.remove(cell)
     word = word[:-1]
 
 
-def n_length_path(n: int, cell: Location, board: Board, words: Iterable[str], word, path, path_lst):
-    """starting from a specific cell iterate over all of its possible paths.
-    when a path is n-long and assembles a word from the dict, add it to the path list"""
-    # Add current cell to the path
-    path.append(cell)
-    word += board[cell[0]][cell[1]]
-    # Success condition - add the path to the path list
-    if len(path) == n and word in words:
-        path_lst.append(path.copy())
-    # Iterate on one of the cell's not-yet-used neighbors
-    if len(path) <= n:
-        for neighbor in all_valid_neighbors(cell, board):
-            if neighbor not in path:
-                n_length_path(n, neighbor, board, words, path, path_lst)
-    # Remove the cell from the path to backtrack
-    path.remove(cell)
-    word = word[:-1]
-
-
-def max_score(cell: Location, board: Board, words:Iterable[str], word, path, path_dict):
+def max_score(cell, board, word, word_dict: dict[int:Iterable[str]], max_l, path, path_dict):
     """starting from a specific cell, iterate and backtrack along all possible path,
     and save in a dictionary the highest score paths which produce valid words"""
     # Add current cell to the path and word
     path.append(cell)
     word += board[cell[0]][cell[1]]
     # Success condition - add the path and word to the dict
-    if word in words:
-        if word not in path_dict.keys() or score(path) > score(path_dict[word]):
-            path_dict[word] = path
+    if word not in path_dict.keys() or score(path) > score(path_dict[word]):
+        if word in word_dict[len(word)]:
+            path_dict[word] = path.copy()
     # Iterate on one of the cell's not-yet-used neighbors
-    for neighbor in all_valid_neighbors(cell, board):
-        if neighbor not in path:
-            max_score(neighbor, board, words, word, path, path_dict)
+    if len(word) <= max_l and (len(word) < 4 or check_word(word, word_dict)):
+        for neighbor in all_valid_neighbors(cell, board):
+            if neighbor not in path:
+                max_score(neighbor, board, word, word_dict, max_l, path, path_dict)
     # Remove the cell from the path and word to backtrack
     path.remove(cell)
     word = word[:-1]
