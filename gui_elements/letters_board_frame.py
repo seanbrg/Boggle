41a35b7372d69b228d77b9ee65763d1b2eae1ec1
Shahar Tamir <shahartamir1@gmail.com>
2023-06-24 13:00:47 +0300

    Add GUI elements

---
+++ gui_elements/letters_board_frame.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from gui_elements.letter_button import LetterButton
+
+class LettersBoardFrame:
+    """Creates a frame with GameButtons for boggle game"""
+    def __init__(self, root, game_buttons):
+        self.letters_frame = tk.Frame(root)
+        self._configure_grid()
+        self.letters_buttons = {}
+        self.init_game_buttons(game_buttons)
+
+    def get_letters_frame(self):
+        return self.letters_frame
+
+    def get_game_buttons(self):
+        return self.letters_buttons
+
+    def _configure_grid(self):
+        for i in range(4):
+            self.letters_frame.columnconfigure(i, weight=1, minsize=60)
+            self.letters_frame.rowconfigure(i, weight=1, minsize=60)
+
+    def init_game_buttons(self, game_buttons):
+        for button in game_buttons:
+            self.letters_buttons[button] = LetterButton(self.letters_frame, game_buttons[button], button)
+            self.letters_buttons[button].position_button()
